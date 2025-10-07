588e51ab67494b161dcd541d45c49adac6488b2a
Your Name <you@example.com>
2025-10-07 10:58:49 +0200

    footer and widgets added

--- functions.php
+++ functions.php
@@ -35,7 +35,7 @@ function my_theme_scripts() {
     
     wp_enqueue_script( 'slider-hero-script', get_template_directory_uri() . '/scripts/slider-hero.js', array(),
        null,
-       true
+       true //change to false if not working
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
@@ -97,3 +97,83 @@ add_theme_support('post-thumbnails');
 
 
 /* -----------------------------------------------------footer------------------------------------------------------------ */
+
+function footer_widgets(){
+
+    for ( $i = 1; $i <= 3; $i++ ) {
+        register_sidebar( array(
+            'name'          => 'Footer Widget Area ' . $i,
+            'id'            => 'footer-' . $i,
+            'before_widget' => '<div class="footer-widget">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="footer-widget-title">',
+            'after_title'   => '</h3>',
+        ) );
+     }
+
+}
+add_action('widgets_init', 'footer_widgets');
+
+/* ---------------------------------------------customized widget -------------------------------------------------------- */
+// === Footer Settings for Customizer ===
+function mytheme_footer_customizer( $wp_customize ) {
+
+    // Add a Footer section
+    $wp_customize->add_section( 'footer_settings_section', array(
+        'title'    => __( 'Footer Settings', 'mytheme' ),
+        'priority' => 130,
+    ) );
+
+    // --- Enable Footer Widgets ---
+    $wp_customize->add_setting( 'enable_footer_widgets', array(
+        'default'           => true,
+        'sanitize_callback' => 'wp_validate_boolean',
+    ) );
+
+    $wp_customize->add_control( 'enable_footer_widgets', array(
+        'label'    => __( 'Display Footer Widgets', 'mytheme' ),
+        'section'  => 'footer_settings_section',
+        'type'     => 'checkbox',
+    ) );
+
+    // --- Show/Hide Social Icons ---
+    $wp_customize->add_setting( 'show_footer_social', array(
+        'default'           => true,
+        'sanitize_callback' => 'wp_validate_boolean',
+    ) );
+
+    $wp_customize->add_control( 'show_footer_social', array(
+        'label'    => __( 'Show Social Media Links', 'mytheme' ),
+        'section'  => 'footer_settings_section',
+        'type'     => 'checkbox',
+    ) );
+
+    // --- Show/Hide Newsletter ---
+    $wp_customize->add_setting( 'show_footer_newsletter', array(
+        'default'           => true,
+        'sanitize_callback' => 'wp_validate_boolean',
+    ) );
+
+    $wp_customize->add_control( 'show_footer_newsletter', array(
+        'label'    => __( 'Show Newsletter Form', 'mytheme' ),
+        'section'  => 'footer_settings_section',
+        'type'     => 'checkbox',
+    ) );
+
+    // --- Optional: Add URL fields for each social network ---
+    $socials = array( 'facebook', 'twitter', 'instagram', 'linkedin' );
+    foreach ( $socials as $social ) {
+        $wp_customize->add_setting( "footer_{$social}_url", array(
+            'default'           => '',
+            'sanitize_callback' => 'esc_url_raw',
+        ) );
+
+        $wp_customize->add_control( "footer_{$social}_url", array(
+            'label'   => sprintf( __( '%s URL', 'mytheme' ), ucfirst( $social ) ),
+            'section' => 'footer_settings_section',
+            'type'    => 'url',
+        ) );
+    }
+}
+add_action( 'customize_register', 'mytheme_footer_customizer' );
+
